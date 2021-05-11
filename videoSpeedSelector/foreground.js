cd2c8abf282aac62ef4d23fb377fe5a3dcd5c269
RyanHelgoth <78457606+RyanHelgoth@users.noreply.github.com>
2021-05-10 21:32:59 -0600

    Added slider for selecting speed

--- videoSpeedSelector/foreground.js
+++ videoSpeedSelector/foreground.js
@@ -4,12 +4,19 @@
 
 //https://stackoverflow.com/a/40666096
 chrome.storage.local.get('speed', function (items) {
-    setVideoSpeed(items.speed);
-    chrome.storage.local.remove('speed');
+    if (typeof items.speed !== 'undefined') { // Prevents errors if slider is moved too fast which stores an undefined value
+        setVideoSpeed(items.speed);
+        chrome.storage.local.remove('speed'); //clear() instead?
+    }
 });
 
 function setVideoSpeed(speed){
+    console.log(speed);
     document.querySelector('video').playbackRate = speed;
+
+    chrome.storage.local.set({
+        lastSpeed: speed
+    });
 }
 
 
--- videoSpeedSelector/popup.js
+++ videoSpeedSelector/popup.js
@@ -1,7 +1,35 @@
 
 
 var confirmButton = document.getElementById("confirmButton");
+var speedSlider = document.getElementById("speedSlider");
+var sliderNum = document.getElementById("sliderNum");
 
+// Speed stays if page is refreshed, should probably use a foreground script to find speed.
+chrome.storage.local.get("lastSpeed", function (items) {
+    if (typeof items.lastSpeed !== 'undefined') { 
+        speedSlider.value = items.lastSpeed;
+        sliderNum.innerText = items.lastSpeed;
+    }
+});
+
+
+
+//https://www.impressivewebs.com/onchange-vs-oninput-for-range-sliders/
+speedSlider.addEventListener('input', function () { //See if i can change to an on release listener
+    var vidSpeed = speedSlider.value;
+    
+    sliderNum.innerText = vidSpeed;
+    
+    chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
+
+
+        //https://stackoverflow.com/a/40666096
+        chrome.storage.local.set({
+            speed: vidSpeed
+        }, executeForegroundScript(tabs)); // Callback invoked after storing is complete.
+        
+    });
+  }, false);
 
 
 
