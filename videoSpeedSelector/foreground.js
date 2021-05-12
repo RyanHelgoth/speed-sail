f4ae9693f734755fa77c502985f2843b9b7af69f
RyanHelgoth <78457606+RyanHelgoth@users.noreply.github.com>
2021-05-12 17:19:00 -0600

    Added support for pages with multiple videos

--- videoSpeedSelector/foreground.js
+++ videoSpeedSelector/foreground.js
@@ -12,14 +12,20 @@ chrome.storage.local.get('speed', function (items) {
 
 function setVideoSpeed(speed){
     console.log(speed);
-    var video = document.querySelector('video');
+    var videos = document.querySelectorAll('video');
+    console.log(videos);
 
-    if (video !== null) {
-        video.playbackRate = speed;
-        chrome.storage.local.set({
-            lastSpeed: speed
-        });
-    }
+
+    videos.forEach(function(video) {
+        if (video !== null) {
+            video.playbackRate = speed;
+        }
+    });
+
+    chrome.storage.local.set({
+        lastSpeed: speed
+    });
+    
     
 
     
--- videoSpeedSelector/popup.js
+++ videoSpeedSelector/popup.js
@@ -86,3 +86,4 @@ function executeForegroundScript(tabs) {
         });
     }
 }
+
