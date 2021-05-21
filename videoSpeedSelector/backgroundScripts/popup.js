ad05047e87567e3183426a8451bbacb9c601f2a8
RyanHelgoth <78457606+RyanHelgoth@users.noreply.github.com>
2021-05-21 15:47:42 -0600

    Finished removal of old features

--- videoSpeedSelector/backgroundScripts/popup.js
+++ videoSpeedSelector/backgroundScripts/popup.js
@@ -28,6 +28,7 @@ function getUiElements() {
 function initializeUi() {
     speedSlider.value = "1";
     sliderNum.innerText = "1";
+    message.innerText = "";
     chrome.storage.local.get("currentSpeed", function (items) {
         if (typeof items.currentSpeed !== "undefined") { 
             appliedSpeed.innerText = items.currentSpeed;
@@ -46,7 +47,7 @@ function setSelectedSpeedText() {
 
 //Adds listeners.
 function addListeners() {
-
+    
     //Catches change in speedSet boolean that tracks if applying speed selection was sucessful or not.
     chrome.storage.onChanged.addListener(displayResults); 
     
@@ -62,29 +63,21 @@ function addListeners() {
 
 //Set speed of videos in current tab to the speed the user has selected.
 function setSpeed() {
-    let selectedSpeed = speedSlider.valueAsNumber;
+    let selectedSpeed = speedSlider.value;
+    
+    chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
+        /*  Link: https://stackoverflow.com/a/40666096
+        Author: Makyen♦
+        Date: Nov 17 '16 at 22:20
+        License: SA 3.0
 
-    chrome.storage.local.get("currentSpeed", function (items) {
-        if (selectedSpeed !== items.currentSpeed) {
-            chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
-                /*  Link: https://stackoverflow.com/a/40666096
-                Author: Makyen♦
-                Date: Nov 17 '16 at 22:20
-                License: SA 3.0
-
-                I used this post to learn how to pass values to content scripts 
-                from background scripts using storage.
-                */
-                chrome.storage.local.set({
-                    selectedSpeed: selectedSpeed
-                }, executeSetSpeedScript(tabs)); 
-            });
-        }
-        else {
-            let times = "\u00D7";
-            message.innerText = "ERROR: Your speed selection of " + selectedSpeed + times + " was not applied, as it is already the current speed.";
-        }
-    });
+        I used this post to learn how to pass values to content scripts 
+        from background scripts using storage.
+        */
+        chrome.storage.local.set({
+            selectedSpeed: selectedSpeed
+        }, executeSetSpeedScript(tabs)); 
+    });  
 }
 
 //Sets slider to speed of 1 and then attempts to apply it to the current tab.
@@ -96,21 +89,25 @@ function setDefaultSpeed() {
 
 //Displays results after an attempt has been made to apply the user's selected speed to videos in the current tab.
 function displayResults() {
-    chrome.storage.local.get("speedSet", function(items) {
-        let choice = speedSlider.value;
-        let times = "\u00D7";
+
+    //Set time out prevents brief display of error message as speedSet is initially set to false.
+    setTimeout(function() {
+        chrome.storage.local.get("speedSet", function(items) {
+            let choice = speedSlider.value;
+            let times = "\u00D7";
         
-        if (items.speedSet) {
-            appliedSpeed.innerText = choice;
-            message.innerText = "Your speed selection of " + choice + times + " has been applied.";
-        }
-        else if (!items.speedSet && typeof items.speedSet !== "undefined") {
-            /*undefined evaluates to false, but when speedSet is undefined it means user has not 
-            tried to apply a speed and the error message below shouldn't be displayed.*/
-            message.innerText = "ERROR: Your speed selection of " + choice + times + " has not been applied as no valid videos were found on this page.";
-        }
-    });
-    chrome.storage.local.remove("speedSet"); //Prevents wrong message being shown on tab switch.
+            if (items.speedSet) {
+                appliedSpeed.innerText = choice;
+                message.innerText = "Your speed selection of " + choice + times + " has been applied.";  
+            }
+            else if (!items.speedSet && typeof items.speedSet !== "undefined") {
+                /*undefined evaluates to false, but when speedSet is undefined it means user has not 
+                tried to apply a speed and the error message below shouldn't be displayed.*/
+                message.innerText = "ERROR: Your speed selection of " + choice + times + " has not been applied as no valid videos were found on this page.";     
+            }
+        });
+        chrome.storage.local.remove("speedSet"); //Prevents wrong message being shown on tab switch.
+    }, 50);
 }
 
 //Executes script responsible for setting video speed in the current tab.
@@ -133,6 +130,7 @@ function executeSetSpeedScript(tabs) {
 
 /*TODO:
 -Uninstall extension and see if current speed causes bugs (when it is undefined)
--Remove old code and add comments
-
+-Update issue on github explaining why im not preventing spam clicks anymore
+-Move executeSetSpeedScript code into setSpeed?
+-Delete unecissary files.
 */
